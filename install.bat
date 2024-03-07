842161e3a170dbe1b25e13629b9c609d9cc59ff0
Lukas <30857678+Lucke0051@users.noreply.github.com>
2024-03-07 12:10:44 +0100

    fix2

--- install.bat
+++ install.bat
@@ -3,13 +3,13 @@
 cd /d "%~dp0"
 
 if not exist Win11.iso (
-	echo "Downloading ISO"
+	echo Downloading ISO
 	curl --output "Win11.iso" --url "https://infomedia.orebro.se/StaticFileHosting/Win11.bin"
 ) else (
-	echo "ISO already exists, using existing"
+	echo ISO already exists, using existing
 )
 
-echo "Setting registry keys"
+echo Setting registry keys
 reg add HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig
 reg add HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig /v BypassTPMCheck /t REG_DWORD /d 1
 reg add HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig /v BypassSecureBootCheck /t REG_DWORD /d 1
@@ -17,14 +17,14 @@ reg add HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig /v BypassSecureBootCheck /t RE
 reg add HKEY_LOCAL_MACHINE\SYSTEM\Setup\MoSetup
 reg add HKEY_LOCAL_MACHINE\SYSTEM\Setup\MoSetup /v AllowUpgradesWithUnsupportedTPMOrCPU /t REG_DWORD /d 1
 
-echo "Bypassing TPM requirement"
+echo Bypassing TPM requirement
 skipTPM.bat
 
-echo "Waiting"
+echo Waiting
 timeout 8
 
-echo "Mounting ISO"
+echo Mounting ISO
 start Win11.iso
 
-echo "Starting setup"
+echo Starting setup
 E:\setup.exe /auto upgrade /dynamicupdate enable /showoobe none /eula accept /copylogs C:\Win11Temp\win11.log
