d04d45fc4207a3397aeb41930c0418844df62969
Lukas <30857678+Lucke0051@users.noreply.github.com>
2024-03-07 11:55:47 +0100

    fix

--- install.bat
+++ install.bat
@@ -1,3 +1,5 @@
+curl --output "Win11.iso" --url "https://infomedia.orebro.se/StaticFileHosting/Win11.bin"
+
 reg add HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig
 reg add HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig /v BypassTPMCheck /t REG_DWORD /d 1
 reg add HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig /v BypassSecureBootCheck /t REG_DWORD /d 1
@@ -5,5 +7,9 @@ reg add HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig /v BypassSecureBootCheck /t RE
 reg add HKEY_LOCAL_MACHINE\SYSTEM\Setup\MoSetup
 reg add HKEY_LOCAL_MACHINE\SYSTEM\Setup\MoSetup /v AllowUpgradesWithUnsupportedTPMOrCPU /t REG_DWORD /d 1
 
-start Win11_23H2_Swedish_x64v2.iso
-E:\setup.exe /auto upgrade /dynamicupdate enable /showoobe none
+skipTPM.bat
+
+timeout 8
+
+start Win11.iso
+E:\setup.exe /auto upgrade /dynamicupdate enable /showoobe none /eula accept
