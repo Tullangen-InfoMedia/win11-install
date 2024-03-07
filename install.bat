35b30af959cae640204d71cb6ae95bab7dc0585b
Lukas <30857678+Lucke0051@users.noreply.github.com>
2024-03-07 11:13:22 +0100

    fix

--- install.bat
+++ install.bat
@@ -1,1 +1,8 @@
-setup.exe /auto upgrade /compat ignorewarning /dynamicupdate disable /showoobe none /eula accept /quiet /noreboot
+reg add HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig
+reg add HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig /v BypassTPMCheck /t REG_DWORD /d 1
+reg add HKEY_LOCAL_MACHINE\SYSTEM\Setup\LabConfig /v BypassSecureBootCheck /t REG_DWORD /d 1
+
+reg add HKEY_LOCAL_MACHINE\SYSTEM\Setup\MoSetup
+reg add HKEY_LOCAL_MACHINE\SYSTEM\Setup\MoSetup /v AllowUpgradesWithUnsupportedTPMOrCPU /t REG_DWORD /d 1
+
+setup.exe /auto upgrade /compat ignorewarning /dynamicupdate enable /showoobe none /eula accept /finalize
