6957d6441bc515605637fc3dca075d165bd3d790
Lukas <30857678+Lucke0051@users.noreply.github.com>
2024-03-07 11:27:56 +0100

    fix

--- install.bat
+++ install.bat
@@ -6,4 +6,4 @@ reg add HKEY_LOCAL_MACHINE\SYSTEM\Setup\MoSetup
 reg add HKEY_LOCAL_MACHINE\SYSTEM\Setup\MoSetup /v AllowUpgradesWithUnsupportedTPMOrCPU /t REG_DWORD /d 1
 
 start Win11_23H2_Swedish_x64v2.iso
-setup.exe /auto upgrade /dynamicupdate enable /showoobe none
+E:\setup.exe /auto upgrade /dynamicupdate enable /showoobe none
